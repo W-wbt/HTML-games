fed97fc30f785a1546020bf795b0907705f5bbc3
王丙泰 <32089072+W-wbt@users.noreply.github.com>
2020-08-17 16:18:19 +0800

    upload src files

---
+++ src/App.vue
@@ -0,0 +1,289 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created () {
+    // 在页面加载时读取sessionStorage里的状态信息
+    if (sessionStorage.getItem('store')) {
+      this.$store.replaceState(Object.assign({}, this.$store.state, JSON.parse(sessionStorage.getItem('store'))))
+    }
+    // 在页面刷新时将vuex里的信息保存到sessionStorage里
+    window.addEventListener('beforeunload', () => {
+      sessionStorage.setItem('store', JSON.stringify(this.$store.state))
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+  background: #EEF0F5;
+}
+* {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  /* color: #333333; */
+}
+html, body {width: 100%;height: 100%;}
+a {outline: none;text-decoration: none;cursor: pointer;}
+input {outline: none;border: 0;}
+input::-moz-placeholder {color: #9B9B9B;}
+input::-ms-input-placeholder {color: #9B9B9B;}
+input::-webkit-input-placeholder {color: #9B9B9B;}
+ul {padding: 0;}
+ul li {list-style: none;list-style-type:none;}
+button {outline: none;border: 0;}
+.bg {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+}
+
+.top_search_part {
+  flex-shrink: 0;
+  height: 72px;
+  padding: 0 32px 0 28px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #FFFFFF;
+  margin-bottom: 12px;
+}
+.search_box {
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+}
+.search_item {
+  margin-left: 40px;
+  display: flex;
+  align-items: center;
+}
+.search_item:first-child {
+  margin-left: 0;
+}
+.search_title {
+  flex-shrink: 0;
+  font-size: 16px;
+  color: #333333;
+  /* margin-right: 10px; */
+}
+/*  */
+.goBackBox {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: default;
+}
+.goBackIcon {
+  width: 21px;
+  height: 21px;
+  margin-right: 16px;
+}
+.goBackTitle {
+  font-size: 18px;
+  color: #333333;
+  line-height: 27px;
+}
+/*  */
+.el-input {
+  /* border: 1px solid #979797; */
+  border-radius: 4px;
+}
+.search_btn {
+  flex-shrink: 0;
+  height: 40px;
+  margin-left: 40px;
+  padding: 0 33px;
+  background: #0066FF;
+  border-radius: 4px;
+  font-size: 16px;
+  line-height: 40px;
+  color: #FFFFFF;
+  cursor: pointer;
+}
+
+.add_btn {
+  flex-shrink: 0;
+  height: 40px;
+  border: 1px solid #0066FF;
+  border-radius: 4px;
+  padding: 0 23px 0 21px;
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  color: #0066FF;
+  cursor: pointer;
+}
+.add_icon {
+  flex-shrink: 0;
+  width: 15.1px;
+  height: 15.1px;
+  margin-right: 4px;
+}
+
+.content {
+  /* height: calc(100% - 84px); */
+  flex-grow: 1;
+  /* display: flex;
+  flex-direction: column;
+  align-items: stretch; */
+  background: #FFFFFF;
+  box-shadow: 0 0 4px 0 rgba(203,203,203,0.50);
+  border-radius: 2px;
+  position: relative;
+}
+.tableBox {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: calc(100% - 40px);
+  /* overflow-y: scroll; */
+  border: 2px solid #FFFFFF;
+}
+/* .tableBox::-webkit-scrollbar { width: 0 !important; }
+.tableBox{ -ms-overflow-style: none; }
+.tableBox { overflow: -moz-scrollbars-none; } */
+.tablePage {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  border-top: 1px solid #E4E4E4;
+}
+
+/* 表格样式 */
+.tableHeader {
+  background: #EEF0F5 !important;
+  font-size: 16px;
+  color: #333333;
+}
+.tableBodyer {
+  font-size: 16px;
+  color: #333333;
+}
+.el-table--striped .el-table__body tr.el-table__row--striped td {
+  background: rgba(238,240,245,0.60) !important;
+}
+.el-table--border td, .el-table--border th, .el-table__body-wrapper .el-table--border.is-scrolling-left~.el-table__fixed {
+  border-right: 1px solid #FFFFFF !important;
+}
+.tableBox .operate_btn_box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.tableBox .operation_btn {
+  width: 20px;
+  height: 20px;
+  margin-right: 20px;
+  background-size: 100% 100%;
+}
+.tableBox .operation_btn:last-child {
+  margin-right: 0;
+}
+.tableBox .updateBtn {
+  background: url(./assets/editIcon.svg) no-repeat center;
+}
+.tableBox .delBtn {
+  background: url(./assets/delIcon.svg) no-repeat center;
+}
+.tableBox .cancelBtn {
+  background: url(./assets/cancellationIcon.svg) no-repeat center;
+}
+.operation_a_blue {
+  font-size: 14px;
+  color: #0066FF;
+}
+
+/* 弹窗样式 */
+.el-dialog__header {
+  background: #0066FF;
+}
+.el-dialog__title, .el-dialog__headerbtn .el-dialog__close {
+  color: #ffffff !important;
+}
+.dialog_box {
+  width: 100%;
+  padding: 0 30px 20px 10px;
+}
+.dialog_row {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.dialog_item {
+  min-width: 50%;
+  flex-shrink: 0;
+  flex-grow: 1;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin: 10px 0 20px;
+}
+.dialog_item_title {
+  flex-shrink: 0;
+  min-width: 120px;
+  padding: 0 10px;
+  font-size: 18px;
+  color: #333333;
+  text-align: right;
+}
+.dialog_item_input {
+  /* flex-shrink: 0; */
+  /* width: 250px !important; */
+}
+.dialog_item_input .el-input__inner {
+  text-align: left;
+}
+.dialog_item_input_text {
+  margin-left: 10px;
+  font-size: 18px;
+  color: #333333;
+}
+/* 弹窗样式 */
+/* 标签页 */
+.tabsBox {
+  width: 100%;
+  height: 43px;
+  margin-bottom: 12px;
+  display: flex;
+  align-items: flex-end;
+  padding-left: 56px;
+}
+.tabsBox .el-tabs__header {
+  margin: 0;
+}
+.tabsBox .el-tabs__nav-wrap::after {
+  background-color: #EEF0F5;
+}
+.tabsBox .el-tabs__item {
+  font-size: 18px;
+  padding: 0 67.5px;
+}
+.tabsBox .el-tabs__item.is-active {
+  color: #0048FF;
+}
+.tabsBox .el-tabs__active-bar {
+  background-color: #0048FF;
+  border-radius: 2px;
+}
+/* 标签页 */
+</style>
---
+++ src/components/fileOperations/readFile.vue
@@ -0,0 +1,219 @@
+<template>
+  <div class="bg">
+    <div class="top_operation_box">
+      <div></div>
+      <div class="right_operation_box">
+        <el-upload class="uploadBtnBox" action="/" :show-file-list="false" :auto-upload="false" :on-change="uploadChange">
+          <el-button class="importBtn" icon="el-icon-upload">导入excel文件</el-button>
+        </el-upload>
+        <el-button class="exportBtn" @click="exportExcel">导出</el-button>
+        <el-button class="exportBtn" @click="filtrate">筛选</el-button>
+      </div>
+    </div>
+    <!-- <el-button class="importBtn" v-if="workList.length !== 0">处理数据</el-button> -->
+    <div class="middle_table_box">
+      <el-table id="workList" :data="workList" style="width: 100%" stripe size="small" header-cell-class-name="tableHeader" cell-class-name="tableBodyer" height="100%" @selection-change="selectionChange">
+        <el-table-column type="selection" width="50"></el-table-column>
+        <el-table-column label="序号" prop="index" width="50"></el-table-column>
+        <el-table-column label="岗位代码" prop="__EMPTY" width="100"></el-table-column>
+        <el-table-column label="用人单位及招考岗位">
+          <el-table-column label="用人单位序号" prop="__EMPTY_1" width="100"></el-table-column>
+          <el-table-column label="用人单位名称" prop="__EMPTY_2" min-width="100"></el-table-column>
+          <el-table-column label="岗位类别" prop="__EMPTY_3" width="100"></el-table-column>
+          <el-table-column label="岗位名称" prop="__EMPTY_4" width="100"></el-table-column>
+          <el-table-column label="从事工作" prop="__EMPTY_5" min-width="100"></el-table-column>
+          <el-table-column label="招考数量" prop="__EMPTY_6" width="60"></el-table-column>
+          <el-table-column label="入围比例" prop="__EMPTY_7" width="60"></el-table-column>
+        </el-table-column>
+        <el-table-column label="招考条件">
+          <el-table-column label="来源类别" prop="__EMPTY_8" min-width="100"></el-table-column>
+          <el-table-column label="学历" prop="__EMPTY_9" min-width="100"></el-table-column>
+          <el-table-column label="学位" prop="__EMPTY_10" min-width="100"></el-table-column>
+          <el-table-column label="所学专业" prop="__EMPTY_11" min-width="120"></el-table-column>
+          <el-table-column label="考试专业科目" prop="__EMPTY_12" width="100"></el-table-column>
+          <el-table-column label="专业技术资格">
+            <el-table-column label="毕业生" prop="__EMPTY_13" width="100"></el-table-column>
+            <el-table-column label="社会人才" prop="__EMPTY_14" width="100"></el-table-column>
+          </el-table-column>
+          <el-table-column label="职业资格">
+            <el-table-column label="毕业生" prop="__EMPTY_15" width="100"></el-table-column>
+            <el-table-column label="社会人才" prop="__EMPTY_16" width="100"></el-table-column>
+          </el-table-column>
+          <el-table-column label="其他条件" prop="__EMPTY_17" min-width="150"></el-table-column>
+        </el-table-column>
+        <el-table-column label="工作地点" prop="__EMPTY_18" width="60"></el-table-column>
+        <el-table-column label="咨询电话" prop="__EMPTY_19" min-width="120"></el-table-column>
+       </el-table>
+    </div>
+  </div>
+</template>
+
+<script>
+  import FileSaver from 'file-saver'
+  import XLSX from 'xlsx'
+  export default {
+    name: 'readFile',
+    data () {
+      return {
+        workList: [],
+        multipleSelection: []
+      }
+    },
+    methods: {
+      uploadChange (file) {
+        this.loading = this.$loading({
+          lock: true,
+          text: 'excle数据处理中...',
+          spinner: 'el-icon-loading',
+          background: 'rgba(0, 0, 0, 0.7)'
+        })
+        let _this = this
+        // console.log(file)
+        const types = file.name.split('.')[1]
+        const fileType = ['xlsx', 'xlc', 'xlm', 'xls', 'xlt', 'xlw', 'csv'].some(item => item === types)
+        if (!fileType) {
+          this.loading.close()
+          this.$message('格式错误，请重新选择！')
+        } else {
+          this.file2Xce(file).then(tabJson => {
+            if (tabJson && tabJson.length > 0) {
+              // this.xlsxJson = tabJson
+              console.log('原始数据长度：', tabJson[0].sheet.slice(4).length)
+              _this.dataProcess(tabJson[0].sheet.slice(4))
+            } else {
+              this.loading.close()
+              this.$message('文件内没有内容！')
+            }
+          })
+        }
+      },
+      file2Xce(file) {
+        return new Promise(function(resolve, reject) {
+          const reader = new FileReader()
+          reader.onload = function(e) {
+            const data = e.target.result
+            this.wb = XLSX.read(data, {
+              type: 'binary'
+            })
+            const result = []
+            this.wb.SheetNames.forEach((sheetName) => {
+              result.push({
+                sheetName: sheetName,
+                sheet: XLSX.utils.sheet_to_json(this.wb.Sheets[sheetName])
+              })
+            })
+            resolve(result)
+          }
+          reader.readAsBinaryString(file.raw)
+          // reader.readAsBinaryString(file) // 传统input方法
+        })
+      },
+      dataProcess (tabJson) {
+        let list = []
+        let one = []
+        let two = []
+        for (let i = 0; i < tabJson.length; i++) {
+          // if (tabJson[i].__EMPTY_8.includes('社会人才') && tabJson[i].__EMPTY_10.includes('学士') && tabJson[i].__EMPTY_11.includes('本科') && tabJson[i].__EMPTY_11.includes('计算机')) {
+          //   list.push(tabJson[i])
+          // }
+          if (tabJson[i].__EMPTY_8.includes('社会人才') && tabJson[i].__EMPTY_10.includes('学士') && tabJson[i].__EMPTY_11.includes('本科')) {
+            if ((tabJson[i].__EMPTY_11.split('本科：')[1].includes('计算机') || tabJson[i].__EMPTY_11.split('本科：')[1].includes('工学')) && !tabJson[i].__EMPTY_17.includes('党员') && !tabJson[i].__EMPTY_17.includes('女性') && !tabJson[i].__EMPTY_17.includes('985') && !tabJson[i].__EMPTY_17.includes('211')) {
+              if (tabJson[i].__EMPTY_14.includes('无要求') && tabJson[i].__EMPTY_16.includes('无要求') && !tabJson[i].__EMPTY_17.includes('数据库') && !tabJson[i].__EMPTY_17.includes('军队')) {
+                if (!tabJson[i].__EMPTY_17.includes('美术') && !tabJson[i].__EMPTY_17.includes('JAVA') && !tabJson[i].__EMPTY_17.includes('人力资源') && !tabJson[i].__EMPTY_17.includes('论文') && !tabJson[i].__EMPTY_17.includes('省户口')) {
+                  if (tabJson[i].__EMPTY_12.includes('数学') && !tabJson[i].__EMPTY_17.includes('医院') && !tabJson[i].__EMPTY_17.includes('多媒体') && !tabJson[i].__EMPTY_17.includes('六级') && !tabJson[i].__EMPTY_17.includes('兵役')) {
+                    // if (!tabJson[i].__EMPTY_17.includes('户口')) {
+                    //   one.push(tabJson[i])
+                    // } else if (tabJson[i].__EMPTY_17.includes('天津')) {专业技术资格
+                    //   two.push(tabJson[i])
+                    // }
+                    if (!tabJson[i].__EMPTY_17.includes('双一流')) {
+                      list.push(tabJson[i])
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        // console.log(one, two)
+        // list = one.concat(two)
+        let index = 0
+        this.workList = list.map(item => {
+          item.index = ++index
+          return item
+        })
+        this.loading.close()
+        // let province = []
+      },
+      // 导出
+      exportExcel () {
+        if (this.workList.length === 0) {
+          this.$message('请先导入excel文件！')
+          return
+        }
+        this.loading = this.$loading({
+          lock: true,
+          text: 'excle数据导出中...',
+          spinner: 'el-icon-loading',
+          background: 'rgba(0, 0, 0, 0.7)'
+        })
+        let name = new Date().getTime() + '.xlsx'
+        /* generate workbook object from table */
+        var table = XLSX.utils.table_to_book(document.querySelector('#workList'))
+        /* get binary string as output */
+        var exportTable = XLSX.write(table, { bookType: 'xlsx', bookSST: true, type: 'array' })
+        try {
+          FileSaver.saveAs(new Blob([exportTable], { type: 'application/octet-stream' }), name)
+        } catch (e) {
+          if (typeof console !== 'undefined') console.log(e, exportTable)
+        }
+        this.loading.close()
+      },
+      // 选择岗位
+      selectionChange (val) {
+        this.multipleSelection = val
+      },
+      filtrate () {
+        if (this.multipleSelection.length === 0) {
+          this.$message('请选择岗位！')
+        } else {
+          this.workList = this.multipleSelection
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.bg {
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: scroll;
+}
+.middle_table_box {
+  height: calc(100% - 70px);
+  height: -moz-calc(100% - 70px);
+  height: -webkit-calc(100% - 70px);
+}
+.uploadBtnBox {
+  flex-shrink: 0;
+}
+.importBtn {
+  width: 200px;
+  height: 48px;
+  font-size: 16px;
+  color: #FFFFFF;
+  letter-spacing: 2px;
+  margin: 0 0 10px;
+}
+.exportBtn {
+  width: 200px;
+  height: 48px;
+  font-size: 16px;
+  color: #FFFFFF;
+  letter-spacing: 2px;
+  margin: 0 0 10px 20px;
+}
+</style>
